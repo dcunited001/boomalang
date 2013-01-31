0fc99b0f544e64a6d6c56911656e7a2b173d776a
David Conner <dconner.pro@gmail.com>
2013-01-31 18:08:24 -0500

    added section on basic IO methods: cout & cin

--- cpp/basics.cpp
+++ cpp/basics.cpp
@@ -7,20 +7,35 @@
 
 // ___________________________
 // add iostream for IO (cout)
-#include <iostream>
+#include <iostream>;
 
 // main()
 // ===========================
 // main is recognized as the entry point for cpp programs
 
-int main()
+
+int main(int argc, char *argv[])
 {
   using namespace std;
   cout << "Hello world!" << endl;
   return 0;
 }
 
-// declaring variables:
+// basic types
 // ===========================
-int xx;
-int yy = 123;
+
+// basic IO
+// ===========================
+// must include <iostream>
+void basicIO()
+{
+  string name;
+
+  // endl supplies end-line to cout??
+  cout << "use cout to print " << endl;
+
+  cout << "enter your name: " << endl;
+  cin >> name;
+
+  cout << "your name is: " << name << endl;
+}
