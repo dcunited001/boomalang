a845b2c0ef762d91afe16b993414a149befc6b5a
David Conner <dconner.pro@gmail.com>
2013-01-31 18:07:44 -0500

    added a few basic c++ functions

--- cpp/functions.cpp
+++ cpp/functions.cpp
@@ -1,2 +1,16 @@
 // Functions
 // ===========================
+// declare basic functions like this:
+
+// void functions return no value
+void egFunc(string param1, string param2)
+{
+  cout << "param1: " << param1 << endl;
+  cout << "param2: " << param2 << endl;
+}
+
+// declaring a return type
+int egIntAdd(int a, int b)
+{
+  return a+b;
+}
