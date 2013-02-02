a4b6339dc559e98f712a35d83723434894a9d55c
David Conner <dconner.pro@gmail.com>
2013-02-02 11:04:01 -0500

    added a file to describe c++ control flow structures (if, for, while, switch, etc)

---
+++ cpp/controlFlow.cpp
@@ -0,0 +1,32 @@
+// Control Flow
+// ===========================
+
+// If Statements
+// ===========================
+
+bool testGreater(int a, int b) {
+  return (a > b);
+}
+
+// Switch Statements
+// ===========================
+
+
+// Goto Statements
+// ===========================
+
+
+// While Statements
+// ===========================
+
+
+// Do While Statements
+// ===========================
+
+
+// For Statements
+// ===========================
+
+
+// Break and Continue
+// ===========================
