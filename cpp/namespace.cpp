995d419876118e9957f9d5761a329c2763dcd139
David Conner <dconner.pro@gmail.com>
2013-02-02 11:15:08 -0500

    added notes and examples for the using keyword in c++

--- cpp/namespace.cpp
+++ cpp/namespace.cpp
@@ -1,22 +1,55 @@
 // Namespaces
 // ===========================
 // http://www.cplusplus.com/doc/tutorial/namespaces/
-#include <iostream>;
+//#include <iostream>;
+//using namespace std;
+
+// ===========================
+// TODO: NEED TO DECIDE HOW TO BEST STRUCTURE THIS PROJECT
+// ===========================
+//#include <basics>
 
 // Creating Namespaces
 // ===========================
-namespace testNamespace
-{
-  int intA="Namespace: int";
-  int strB="Namespace: int";
+namespace testNamespace {
+  int intA=123;
+  string strB="Namespace: string";
+}
+
+// Using Namespaces
+// ===========================
+// the using keyword introduces a name from a namespace
+//   into the current declarative region.
+
+namespace first {
+  int x = 10;
+  int y = 20;
+}
+
+namespace second {
+  int x = -100;
+  int y = -100;
+}
+
+void testUsing() {
+  using namespace first;
+
+  // since we're using the first namespace
+  //   no need to qualify x with first::x
+  cout << x << endl;
+  cout << y << endl;
+
+  // but since we're not using the second namespace
+  //   still need to qualify to second::x
+  cout << second::x << endl;
+  cout << second::y << endl;
 }
 
 // Accessing Namespaces
 // ===========================
 // use :: to access vars from a separate namespace
 
-void printNamespaceVars()
-{
-  cout << testNamespace::a;
-  cout << testNamespace::b;
+void printNamespaceVars(){
+  cout << testNamespace::intA;
+  cout << testNamespace::strB;
 }
